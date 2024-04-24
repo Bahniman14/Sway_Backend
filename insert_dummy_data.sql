f032a5c4b035c6d21f49fe17bb47567cf7081d51
Bahniman <bahniman.rkm@gmail.com>
2024-04-24 16:05:19 +0530

    "Database is ready"

--- insert_dummy_data.sql
+++ insert_dummy_data.sql
@@ -1,27 +1,5 @@
--- SQL: USER TABLE
-INSERT INTO user_table (user_id, username, isSubscribed, channels_created, channel_subscribed, polls, followers, followings, photoURL, subscription_data, about_me, visibility, score)
-VALUES ('U001', 'RajeshKumar', 0, NULL,'CH001','P005,P009', NULL, 'U004', 'PH001', NULL, 'Movie Lover', 1, 0),
-('U002', 'PriyaVerma', 0,'CH001', NULL,'P001,P008', 'U003, U004, U005 ', 'U003, U004','PH002',NULL, 'Studious Girl', 1, 0),
-('U003', 'ArjunSingh', 0, NULL,'CH001', 'P002,P004', 'U002, U005', 'U002, U004','PH003',NULL, 'Football Guy', 1, 0),
-('U004', 'NehaPatel', 0, NULL, NULL, 'P003,P006', 'U001, U002, U003, U005', NULL, 'PH004', NULL, 'Influencer', 1, 0),
-('U005', 'RohanSharma', 0, NULL,'CH001', 'P007,P010', NULL,'U002, U003, U004','PH005',NULL, 'GK Boy', 1, 0);
-
-
--- POLL TABLE
-INSERT INTO poll_table (poll_id, question, options, tags, user_created, created_time, created_date, ends_at, comment_list)
-VALUES 
-('P001', 'What is the atomic number of carbon?', 'OP011, OP012, OP013, OP014', 'Science', 'PriyaVerma', '08:00:00', '2024-04-17', '2024-05-01', NULL),
-('P002', 'Which planet is known as the "Red Planet"?', 'OP021, OP022, OP023, OP024', 'Space', 'ArjunSingh', '09:00:00', '2024-04-17', '2024-05-01', NULL),
-('P003', 'Which company is the worlds largest online retailer by revenue?', 'OP031, OP032, OP033, OP034', 'Business', 'NehaPatel', '10:00:00', '2024-04-17', '2024-05-01', 'ArjunSingh'),
-('P004', 'Who won the FIFA World Cup in 2018?', 'OP041, OP042, OP043, OP044', 'Football', 'ArjunSingh', '11:00:00', '2024-04-17', '2024-05-01', NULL),
-('P005', 'What is the capital city of Australia?', 'OP051, OP052, OP053, OP054', 'Geography', 'RajeshKumar', '12:00:00', '2024-04-17', '2024-05-01', NULL),
-('P006', 'Which company is leading the development of self-driving cars?', 'OP061, OP062, OP063, OP064', 'Technology', 'NehaPatel', '13:00:00', '2024-04-17', '2024-05-01', NULL),
-('P007', 'What is the main greenhouse gas responsible for global warming?', 'OP071, OP072, OP073, OP074', 'Environment', 'RohanSharma', '14:00:00', '2024-04-17', '2024-05-01', 'PriyaVerma'),
-('P008', 'Which vaccine has been developed to combat COVID-19?', 'OP081, OP082, OP083, OP084', 'Health', 'PriyaVerma', '15:00:00', '2024-04-17', '2024-05-01', NULL),
-
--- Option TABLE:
-INSERT INTO option_table (option_id, option)
-VALUES 
+INSERT INTO option_table (option_id, options)
+VALUES
 ('OP011', '6'),
 ('OP012', '12'),
 ('OP013', '14'),
@@ -63,9 +41,41 @@ VALUES
 ('OP103', 'Rome'),
 ('OP104', 'Paris');
 
+INSERT INTO poll_table 
+(poll_id, question,                                                          options,                       tags,       user_created, created_time, created_date, ends_at, comment_list)
+VALUES 
+('P001', 'What is the atomic number of carbon?',                            'OP011, OP012, OP013, OP014', 'Science',    'PriyaVerma', '08:00:00', '2024-04-17', '2024-05-01', NULL),
+('P002', 'Which planet is known as the "Red Planet"?',                      'OP021, OP022, OP023, OP024', 'Space',      'ArjunSingh', '09:00:00', '2024-04-17', '2024-05-01', NULL),
+('P003', 'Which company is the worlds largest online retailer by revenue?', 'OP031, OP032, OP033, OP034', 'Business',   'NehaPatel', '10:00:00', '2024-04-17', '2024-05-01', 'CM001'),
+('P004', 'Who won the FIFA World Cup in 2018?',                             'OP041, OP042, OP043, OP044', 'Football',   'ArjunSingh', '11:00:00', '2024-04-17', '2024-05-01', NULL),
+('P005', 'What is the capital city of Australia?',                          'OP051, OP052, OP053, OP054', 'Geography',  'RajeshKumar', '12:00:00', '2024-04-17', '2024-05-01', NULL),
+('P006', 'Which company is leading the development of self-driving cars?',  'OP061, OP062, OP063, OP064', 'Technology', 'NehaPatel', '13:00:00', '2024-04-17', '2024-05-01', NULL),
+('P007', 'What is the main greenhouse gas responsible for global warming?', 'OP071, OP072, OP073, OP074', 'Environment', 'RohanSharma', '14:00:00', '2024-04-17', '2024-05-01', 'CM002'),
+('P008', 'Which vaccine has been developed to combat COVID-19?',            'OP081, OP082, OP083, OP084', 'Health',       'PriyaVerma', '15:00:00', '2024-04-17', '2024-05-01', NULL),
+('P009', 'Who won the Oscar for Best Picture in 2021?',                     'OP091, OP092, OP093, OP094', 'Entertainment', 'RajeshKumar', '12:00:00', '2024-04-17', '2024-05-01', 'CM003'),
+('P010', 'What is the capital city of France?',                             'OP101, OP102, OP103, OP104', 'Education',     'RohanSharma', '12:00:00', '2024-04-17', '2024-05-01', NULL);
 
--- Channel TABLE:
 
-INSERT INTO channel_table (channel_id, channel_name, subscriber_count, polls_in_it)
-VALUES ('CH001', 'Basic Science', 3, 'P001,P008');
 
+INSERT INTO channel_table 
+(channel_id, channel_name, subscriber_count, polls_in_it) 
+VALUES 
+('CH001', 'Basic Science', 3, 'P001, P008');
+
+INSERT INTO user_table 
+(user_id, username, isSubscribed, channels_created, channel_subscribed, polls,      followers,               followings,        photoURL, subscription_data, about_me, visibility, score)
+VALUES 
+('U001', 'RajeshKumar',     0,      NULL,               'CH001',      'P005,P009',  NULL,                   'U004',             'PH001',    NULL,           'Movie Lover', 1, 0),
+('U002', 'PriyaVerma',      0,      'CH001',              NULL,       'P001,P008', 'U003, U004, U005',      'U003, U004',       'PH002',    NULL,           'Studious Girl', 1, 0),
+('U003', 'ArjunSingh',      0,      NULL,               'CH001',      'P002,P004', 'U002, U005',            'U002, U004',       'PH003',    NULL,           'Football Guy', 1, 0),
+('U004', 'NehaPatel',       0,      NULL,                 NULL,       'P003,P006', 'U001, U002, U003, U005', NULL,              'PH004',    NULL,           'Influencer', 1, 0),
+('U005', 'RohanSharma',     0,      NULL,               'CH001',      'P007,P010',  NULL,                   'U002, U003, U004', 'PH005',    NULL,           'GK Boy', 1, 0);
+
+
+
+INSERT INTO comment_table
+(comment_id, user_created, comment)
+VALUES
+('CM001', 'ArjunSingh', 'I dont know'),
+('CM002', 'PriyaVerma', 'All of them'),
+('CM003', 'PriyaVerma', 'Nomadland');
--- schema.sql
+++ schema.sql
@@ -1,48 +1,24 @@
--- Create user_table
-CREATE TABLE user_table (
-    user_id TEXT PRIMARY KEY,
-    username TEXT,
-    -- isSubscribed INTEGER, -- 0 or 1 for boolean value
-    channels_created TEXT REFERENCES channel_table(channel_id), -- Comma-separated text containing channel_ids
-    channel_subscribed TEXT, -- Comma-separated text containing subscribed channel_ids
-    polls TEXT REFERENCES poll_table(poll_id), -- Comma-separated text containing created poll_ids
-    followers TEXT, -- Comma-separated text containing user_ids of followers
-    followings TEXT, -- Comma-separated text containing user_ids of users being followed
-    photoURL TEXT,
-    -- subscription_data TEXT,
-    about_me TEXT,
-    visibility INTEGER, -- 0 or 1 for boolean value
-    score INTEGER Default 0
+-- Create option_table
+CREATE TABLE option_table (
+    option_id TEXT PRIMARY KEY,
+    options TEXT,
+    counts INTEGER DEFAULT 0 -- Default value set to 0
 );
-
-CREATE TABLE organization_table(
-    organization_id TEXT PRIMARY KEY,
-    organization_name TEXT,
-    organization_type TEXT,
-    org_location TEXT,
-    channels_created TEXT REFERENCES channel_table(channel_id), -- Comma-separated text containing channel_ids
-    channel_subscribed TEXT, -- Comma-separated text containing subscribed channel_ids
-    polls TEXT REFERENCES poll_table(poll_id), -- Comma-separated text containing created poll_ids
-    followers TEXT, -- Comma-separated text containing user_ids of followers
-    followings TEXT, -- Comma-separated text containing user_ids of users being followed
-    photoURL TEXT,
-    subscription_data TEXT,
-    about_me TEXT,
-)
-
--- Create channel_table
-CREATE TABLE channel_table (
-    channel_id TEXT PRIMARY KEY,
-    channel_name TEXT,
-    subscriber_count INTEGER DEFAULT 0,
-    polls_in_it TEXT REFERENCES poll_table(poll_id) -- Comma-separated text containing poll_ids
+-- Create comment_table
+CREATE TABLE comment_table (
+    comment_id TEXT PRIMARY KEY, -- Primary key constraint added
+    user_created TEXT,
+    comment TEXT,
+    like_count INTEGER DEFAULT 0,
+    dislike_count INTEGER DEFAULT 0,
+    user_liked TEXT,
+    user_disliked TEXT
 );
-
 -- Create poll_table
 CREATE TABLE poll_table (
     poll_id TEXT PRIMARY KEY,
     question TEXT,
-    options TEXT REFERENCES option_table(option_id), -- Comma-separated text containing option_ids
+    options TEXT, -- Comma-separated text containing option_ids
     tags TEXT,
     user_created TEXT,
     created_time TIME,
@@ -53,24 +29,44 @@ CREATE TABLE poll_table (
     user_interacted INTEGER DEFAULT 0,
     up_vote_count INTEGER DEFAULT 0,
     down_vote_count INTEGER DEFAULT 0,
-    comment_list TEXT REFERENCES comment_table(comment_id) -- Comma-separated text containing comment_ids
+    comment_list TEXT  -- Comma-separated text containing comment_ids
+);
+-- Create channel_table
+CREATE TABLE channel_table (
+    channel_id TEXT PRIMARY KEY,
+    channel_name TEXT,
+    subscriber_count INTEGER DEFAULT 0,
+    polls_in_it TEXT  -- Comma-separated text containing poll_ids
 );
 
-
--- Create option_table
-CREATE TABLE option_table (
-    option_id TEXT PRIMARY KEY,
-    option TEXT,
-    counts INTEGER DEFAULT 0 -- Default value set to 0
+-- Create user_table
+CREATE TABLE user_table (
+    user_id TEXT PRIMARY KEY,
+    username TEXT,
+    isSubscribed INTEGER, -- 0 or 1 for boolean value
+    channels_created TEXT, -- Comma-separated text containing channel_ids
+    channel_subscribed TEXT, -- Comma-separated text containing subscribed channel_ids
+    polls TEXT , -- Comma-separated text containing created poll_ids
+    followers TEXT, -- Comma-separated text containing user_ids of followers
+    followings TEXT, -- Comma-separated text containing user_ids of users being followed
+    photoURL TEXT,
+    subscription_data TEXT,
+    about_me TEXT,
+    visibility INTEGER, -- 0 or 1 for boolean value
+    score INTEGER Default 0
 );
 
--- Create comment_table
-CREATE TABLE comment_table (
-    comment_id TEXT PRIMARY KEY, -- Primary key constraint added
-    user_created TEXT,
-    comment TEXT,
-    like_count INTEGER DEFAULT 0,
-    dislike_count INTEGER DEFAULT 0,
-    user_liked TEXT,
-    user_disliked TEXT
+CREATE TABLE organization_table(
+    organization_id TEXT PRIMARY KEY,
+    organization_name TEXT,
+    organization_type TEXT,
+    org_location TEXT,
+    channels_created TEXT, -- Comma-separated text containing channel_ids
+    channel_subscribed TEXT, -- Comma-separated text containing subscribed channel_ids
+    polls TEXT, -- Comma-separated text containing created poll_ids
+    followers TEXT, -- Comma-separated text containing user_ids of followers
+    followings TEXT, -- Comma-separated text containing user_ids of users being followed
+    photoURL TEXT,
+    subscription_data TEXT,
+    about_me TEXT
 );
